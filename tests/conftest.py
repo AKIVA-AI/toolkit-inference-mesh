aa0af9008ab6fcd71c68254109a11e4863cf3a82
CJ <operations@akivadigital.com>
2026-02-20 23:59:29 -0500

    Fix conftest.py: use collection_path instead of deprecated path parameter

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ REQUIRED_MODULES = ("mlx", "mlx_lm", "zmq")
 MISSING_MODULES = [name for name in REQUIRED_MODULES if importlib.util.find_spec(name) is None]
 
 
-def pytest_ignore_collect(path, config):
+def pytest_ignore_collect(collection_path, config):
     if MISSING_MODULES:
-        filename = getattr(path, "basename", None)
+        filename = getattr(collection_path, "name", None)
         if filename and filename != "test_dependency_check.py":
             return True
     return False
